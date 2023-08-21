cf22ad473e7c03f3504c9447daaca443288290fd
alpheratz0 <alpheratz99@protonmail.com>
2023-08-21 18:47:37 -0300

    format config.mk

--- config.mk
+++ config.mk
@@ -1,8 +1,8 @@
 # Copyright (C) 2022 <alpheratz99@protonmail.com>
 # This program is free software.
 
-CC      = cc
-INCS    = -I/usr/X11R6/include
-CFLAGS  = -std=c99 -pedantic -Wall -Wextra -Os $(INCS)
-LDLIBS  = -lxcb -lxcb-image -lxcb-keysyms
-LDFLAGS = -L/usr/X11R6/lib -s
+CC=cc
+INCS=-I/usr/X11R6/include
+CFLAGS=-std=c99 -pedantic -Wall -Wextra -Os $(INCS)
+LDLIBS=-lxcb -lxcb-image -lxcb-keysyms
+LDFLAGS=-L/usr/X11R6/lib -s
